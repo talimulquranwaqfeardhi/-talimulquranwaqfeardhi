36c3a6eeee9f6ae02d6f7fea6c6aaf152129b4a3
[salim] <[salimademola3@gmail.com]>
2026-07-09 00:40:19 +0100

    Fix MainLayout imports

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '@/components/layout/Navbar';
-import Footer from '@/components/layout/Footer';
+import Navbar from '@/components/home/Navbar';
+import Footer from '@/components/home/Footer';
 
 const MainLayout = () => {
   return (
